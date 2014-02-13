dc39cd6e7ae17645d5eb9c1bed7210b42728a802
gallery1 <gallery1@ws-gallery1>
2014-02-13 14:49:36 +0800

    fdgdfgdf

--- fzsdf/src/com/example/fzsdf/MainActivity.java
+++ fzsdf/src/com/example/fzsdf/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);    //hjhjhbcc
         setContentView(R.layout.activity_main);
     }
 
